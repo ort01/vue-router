8517bd1b9e90f65ee764b1063f985153890e3bb2
ort01 <ortutova.kristina@gmail.com>
2023-01-07 20:14:43 +0100

    Fetching Data

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,29 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The Job ID is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The Job ID is {{ id }}</p>
+    <p>Details of the job: {{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading....</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "JobDetails",
-  props: ["id"],
+  props: ["id", "job"],
   data() {
     return {
       //   id: this.$route.params.id,
+      job: null,
     };
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((error) => console.log(error.message));
   },
   methods: {},
 };
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
   </div>
 </template>
 
@@ -11,14 +16,16 @@
 export default {
   name: "Jobs",
   props: {},
-  data: () => {
+  data() {
     return {
-      jobs: [
-        { title: "UX Designer", id: 1, details: "designer" },
-        { title: "Web Developer", id: 2, details: "developer" },
-        { title: "Vue Developer", id: 3, details: "front-end developer" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((error) => console.log(error.message));
   },
   methods: {},
 };
